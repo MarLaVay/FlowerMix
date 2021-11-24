a9027fd1c386278876cf4d3f5239c2691c6f7612
Farrowlia <79092276+Farrowlia@users.noreply.github.com>
2021-11-24 21:31:57 +0100

    petites retouches esthétiques + bouton reset
    css des backgrounds et des boutons

--- src/components/FlowerMixTable.js
+++ src/components/FlowerMixTable.js
@@ -1,27 +1,26 @@
-
 function FlowerMixTable({flowers, setNewFlower}) {
 
     function addFlower() {
-        const currentScale = Math.random()*1.2 + .15
+        const currentScale = Math.random() * 1.2 + .15
         const currentPetalColor = `rgb(${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)})`
         const newFlowerStyle = {
-            background: `rgb(${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)})`,
-            // height: Math.floor(Math.random()*350),
-            // width: Math.floor(10+Math.random()*300),
-            transform: `scale(${currentScale}, ${currentScale}) rotate(${Math.floor(Math.random()*180)}deg)`,
-            position: 'absolute',
-            top: `${Math.floor(Math.random() * 100)}%`,
-            left: `${Math.floor(Math.random() * 100)}%`,
-            borderRadius: '50%',
-            /*  pétales  */
-            boxShadow:
-                `15px 25px ${currentPetalColor},
+                background: `rgb(${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)}, ${Math.floor(Math.random() * 250)})`,
+                // height: Math.floor(Math.random()*350),
+                // width: Math.floor(10+Math.random()*300),
+                transform: `scale(${currentScale}, ${currentScale}) rotate(${Math.floor(Math.random() * 180)}deg)`,
+                position: 'absolute',
+                top: `${Math.floor(Math.random() * 100)}%`,
+                left: `${Math.floor(Math.random() * 100)}%`,
+                borderRadius: '50%',
+                /*  pétales  */
+                boxShadow:
+                    `15px 25px ${currentPetalColor},
                 -15px 25px ${currentPetalColor},
                 -30px -7px ${currentPetalColor},
                 0 -30px ${currentPetalColor},
                 30px -7px ${currentPetalColor}`
 
-    }
+            }
         ;
         setNewFlower([...flowers, newFlowerStyle])
 
@@ -29,8 +28,11 @@ function FlowerMixTable({flowers, setNewFlower}) {
 
     return (
         <div className='flowerMixTable'>
-            <button onClick={() =>
+            <button className="btn glass-effect" onClick={() =>
                 addFlower()}>Nouvelle fleur
+            </button>
+            <button className="btn glass-effect" onClick={() =>
+                setNewFlower([])}>Tout couper !
             </button>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sytles.css';
+import './styles.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
